a75744dfe935f347f6ca57053bea0827a7da9514
tk-yoshimura <yoshimura8192@gmail.com>
2021-12-25 14:11:47 +0900

    + fgdiff

---
+++ FresnelIntegral/FGDiffSeries.cs
@@ -0,0 +1,26 @@
+﻿using MultiPrecision;
+using System;
+using System.Collections.Generic;
+
+namespace FresnelIntegral {
+    internal static class FGDiffSeries<N> where N : struct, IConstant {
+        public static (MultiPrecision<N>[] fs, MultiPrecision<N>[] gs) Diff(MultiPrecision<N> x, MultiPrecision<N> f, MultiPrecision<N> g, int n) {
+            if (n < 2) {
+                throw new ArgumentOutOfRangeException(nameof(n));
+            }
+
+            List<MultiPrecision<N>> fs = new() { f, -MultiPrecision<N>.PI * x * g };
+            List<MultiPrecision<N>> gs = new() { g, +MultiPrecision<N>.PI * x * f - 1 };
+
+            for (int i = 2; i <= n; i++) {
+                MultiPrecision<N> df = -MultiPrecision<N>.PI * ((i - 1) * gs[i - 2] + x * gs[i - 1]);
+                MultiPrecision<N> dg = +MultiPrecision<N>.PI * ((i - 1) * fs[i - 2] + x * fs[i - 1]);
+
+                fs.Add(df);
+                gs.Add(dg);
+            }
+
+            return (fs.ToArray(), gs.ToArray());
+        }
+    }
+}
--- FresnelIntegral/Program.cs
+++ FresnelIntegral/Program.cs
@@ -4,19 +4,19 @@ using System;
 namespace FresnelIntegral {
     internal class Program {
         static void Main() {
-            (var m, var n) = PadeSolver<Pow2.N8>.Solve(new MultiPrecision<Pow2.N8>[] {
-                1,
-                1,
-                MultiPrecision<Pow2.N8>.Rcp(2),
-                MultiPrecision<Pow2.N8>.Rcp(6),
-                MultiPrecision<Pow2.N8>.Rcp(24),
-                MultiPrecision<Pow2.N8>.Rcp(120),
-                MultiPrecision<Pow2.N8>.Rcp(720),
-                MultiPrecision<Pow2.N8>.Rcp(5040),
-                MultiPrecision<Pow2.N8>.Rcp(40320),
-                MultiPrecision<Pow2.N8>.Rcp(362880),
-                MultiPrecision<Pow2.N8>.Rcp(3628800),
-            }, 5, 5);
+            MultiPrecision<Pow2.N16> x = 2;
+
+            (MultiPrecision<Pow2.N16> f, MultiPrecision<Pow2.N16> g) = FGExpandN16.Value(x);
+
+            (MultiPrecision<Pow2.N16>[] fs, MultiPrecision<Pow2.N16>[] gs) = FGDiffSeries<Pow2.N16>.Diff(x, f, g, 6);
+
+            for (int i = 0; i < fs.Length; i++) {
+                Console.WriteLine(fs[i]);
+            }
+
+            for (int i = 0; i < gs.Length; i++) {
+                Console.WriteLine(gs[i]);
+            }
 
             Console.WriteLine("END");
             Console.Read();

b6629636713958d95df5e291ee88d44bc972247d
tk-yoshimura <yoshimura8192@gmail.com>
2022-02-26 16:01:51 +0900

    + n32

---
+++ FresnelIntegral/FGExpandN32.cs
@@ -0,0 +1,31 @@
+﻿using MultiPrecision;
+using System;
+
+namespace FresnelIntegral {
+    internal static class FGExpandN32 {
+        private const double threshold = 32;
+
+        public static (MultiPrecision<Pow2.N32> f, MultiPrecision<Pow2.N32> g) Value(MultiPrecision<Pow2.N32> x) {
+            if (x.Sign == Sign.Minus) {
+                throw new ArgumentOutOfRangeException(nameof(x));
+            }
+
+            if (x <= threshold) {
+                MultiPrecision<Pow2.N32> fc = NearZero<Pow2.N32, Pow2.N128>.FresnelC(x, max_terms: 2048);
+                MultiPrecision<Pow2.N32> fs = NearZero<Pow2.N32, Pow2.N128>.FresnelS(x, max_terms: 2048);
+
+                MultiPrecision<Pow2.N32> theta = x * x / 2;
+                MultiPrecision<Pow2.N32> cos = MultiPrecision<Pow2.N32>.CosPI(theta);
+                MultiPrecision<Pow2.N32> sin = MultiPrecision<Pow2.N32>.SinPI(theta);
+
+                MultiPrecision<Pow2.N32> f = ((1 - 2 * fs) * cos - (1 - 2 * fc) * sin) / 2;
+                MultiPrecision<Pow2.N32> g = ((1 - 2 * fs) * sin + (1 - 2 * fc) * cos) / 2;
+
+                return (f, g);
+            }
+            else {
+                return Limit<Pow2.N32>.Coef(x);
+            }
+        }
+    }
+}
--- FresnelIntegral/Program.cs
+++ FresnelIntegral/Program.cs
@@ -1,78 +1,133 @@
 ﻿using MultiPrecision;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace FresnelIntegral {
     internal class Program {
         static void Main() {
-            MultiPrecision<Pow2.N16> bin = MultiPrecision<Pow2.N16>.Ldexp(1, -2);
+            for (double x = 16; x <= 20; x += 1d / 16) {
+                MultiPrecision<Pow2.N16> y = FresnelN16.FresnelS(x);
 
-            using (StreamWriter sw = new("../../../../results/pade_table.csv")) {
+                Console.WriteLine($"\"{y:e40}\",");
+            }
 
-                for (MultiPrecision<Pow2.N16> x = bin; x <= 12; x += bin) {
-                    (MultiPrecision<Pow2.N16> f, MultiPrecision<Pow2.N16> g) = FGExpandN16.Value(x);
 
-                    for (int n = 4; n <= 24; n += 1) {
-                        sw.WriteLine($"x={x}, n={n}");
-                        sw.WriteLine($",i,p_i,q_i");
+            static (MultiPrecision<Pow2.N16> f, MultiPrecision<Pow2.N16> g) invg16(MultiPrecision<Pow2.N16> x) {
+                MultiPrecision<Pow2.N16> v = MultiPrecision<Pow2.N16>.Pow2(x);
+                (MultiPrecision<Pow2.N16> f, MultiPrecision<Pow2.N16> g) = FGExpandN16.Value(v);
 
-                        (MultiPrecision<Pow2.N16>[] fs, MultiPrecision<Pow2.N16>[] gs) = FGDiffSeries<Pow2.N16>.Diff(x, f, g, 2 * n);
+                f = MultiPrecision<Pow2.N16>.Log2(f);
+                g = MultiPrecision<Pow2.N16>.Log2(g);
+            
+                return (f, g);
+            };
 
-                        for (int i = 0; i <= 2 * n; i++) {
-                            fs[i] *= MultiPrecision<Pow2.N16>.TaylorSequence[i];
-                            gs[i] *= MultiPrecision<Pow2.N16>.TaylorSequence[i];
+             static (MultiPrecision<Pow2.N32> f, MultiPrecision<Pow2.N32> g) invg32(MultiPrecision<Pow2.N32> x) {
+                MultiPrecision<Pow2.N32> v = MultiPrecision<Pow2.N32>.Pow2(x);
+                (MultiPrecision<Pow2.N32> f, MultiPrecision<Pow2.N32> g) = FGExpandN32.Value(v);
+
+                f = MultiPrecision<Pow2.N32>.Log2(f);
+                g = MultiPrecision<Pow2.N32>.Log2(g);
+            
+                return (f, g);
+            };
+
+            using (StreamWriter sw = new("../../../../results/fresnel_log2log2_fg.csv")) {
+                sw.WriteLine("x,f,g");
+
+                for (double x = 0; x <= 8; x += 1d / 32) {
+                    (MultiPrecision<Pow2.N16> f, MultiPrecision<Pow2.N16> g) = invg16(x);
+
+                    sw.WriteLine($"{x},{f},{g}");
+
+                    Console.WriteLine($"{x}\t{f}\t{g}");
+                }
+            }
+                        
+            using (StreamWriter sw = new("../../../../results/fresnel_log2log2_pade_table.csv")) {
+                for (MultiPrecision<Pow2.N16> x = 0; x <= 4; x += 0.5d) {
+
+                    bool is_e31 = false;
+
+                    MultiPrecision<Pow2.N16> ddx = Math.ScaleB(1, -2);
+                    MultiPrecision<Pow2.N32>[] fds = FiniteDifference<Pow2.N32>.Diff(
+                        x.Convert<Pow2.N32>(), (x) => invg32(x).f, Math.ScaleB(1, -24)
+                    );
+                    MultiPrecision<Pow2.N32>[] gds = FiniteDifference<Pow2.N32>.Diff(
+                        x.Convert<Pow2.N32>(), (x) => invg32(x).g, Math.ScaleB(1, -24)
+                    );
+
+                    List<MultiPrecision<Pow2.N16>> fexpecteds = new(), gexpecteds = new();
+
+                    for (MultiPrecision<Pow2.N16> dx = -ddx, h = ddx / 512; dx <= ddx; dx += h) {
+                        (MultiPrecision<Pow2.N16> fexpected, MultiPrecision<Pow2.N16> gexpected)
+                            = invg16(x + dx);
+
+                        fexpecteds.Add(fexpected);
+                        gexpecteds.Add(gexpected);
+                    }
+
+                    for (int n = 4; n <= 16; n += 1) {
+                        MultiPrecision<Pow2.N16>[] fcs = new MultiPrecision<Pow2.N16>[n * 2 + 1];
+                        MultiPrecision<Pow2.N16>[] gcs = new MultiPrecision<Pow2.N16>[n * 2 + 1];
+                            
+                        (fcs[0], gcs[0]) = invg16(x);
+                        for (int i = 0; i < n * 2; i++) {
+                            fcs[i + 1] = fds[i].Convert<Pow2.N16>() * MultiPrecision<Pow2.N16>.TaylorSequence[i + 1];
+                        }
+                        for (int i = 0; i < n * 2; i++) {
+                            gcs[i + 1] = gds[i].Convert<Pow2.N16>() * MultiPrecision<Pow2.N16>.TaylorSequence[i + 1];
                         }
 
-                        (MultiPrecision<Pow2.N16>[] fms, MultiPrecision<Pow2.N16>[] fns) = PadeSolver<Pow2.N16>.Solve(fs, n, n);
-                        (MultiPrecision<Pow2.N16>[] gms, MultiPrecision<Pow2.N16>[] gns) = PadeSolver<Pow2.N16>.Solve(gs, n, n);
+                        (MultiPrecision<Pow2.N16>[] fms, MultiPrecision<Pow2.N16>[] fns) = PadeSolver<Pow2.N16>.Solve(fcs, n, n);
+                        (MultiPrecision<Pow2.N16>[] gms, MultiPrecision<Pow2.N16>[] gns) = PadeSolver<Pow2.N16>.Solve(gcs, n, n);
 
-                        for (int i = 0; i <= n; i++) {
-                            sw.WriteLine($"f(x),{i},{fms[i]:e64},{fns[i]:e64}");
+                        MultiPrecision<Pow2.N16> err = 0;
+
+                        for ((MultiPrecision<Pow2.N16> dx, MultiPrecision<Pow2.N16> h, int i) = (-ddx, ddx / 512, 0); i < fexpecteds.Count; dx += h, i++) {
+                            MultiPrecision<Pow2.N16> expected = fexpecteds[i];
+                            MultiPrecision<Pow2.N16> actual = PadeSolver<Pow2.N16>.Approx(dx, fms, fns);
+
+                            err = MultiPrecision<Pow2.N16>.Max(err, MultiPrecision<Pow2.N16>.Abs(expected / actual - 1));
                         }
-                        for (int i = 0; i <= n; i++) {
-                            sw.WriteLine($"g(x),{i},{gms[i]:e64},{gns[i]:e64}");
+
+                        for ((MultiPrecision<Pow2.N16> dx, MultiPrecision<Pow2.N16> h, int i) = (-ddx, ddx / 512, 0); i < gexpecteds.Count; dx += h, i++) {
+                            MultiPrecision<Pow2.N16> expected = gexpecteds[i];
+                            MultiPrecision<Pow2.N16> actual = PadeSolver<Pow2.N16>.Approx(dx, gms, gns);
+
+                            err = MultiPrecision<Pow2.N16>.Max(err, MultiPrecision<Pow2.N16>.Abs(expected / actual - 1));
                         }
 
-                        MultiPrecision<Pow2.N16> f_err = 0, g_err = 0;
+                        Console.WriteLine($"x={x}, n={n}, |dx| = {ddx}");
+                        Console.WriteLine($"relative error = {err:e10}");
 
-                        for (MultiPrecision<Pow2.N16> dx = -bin / 2, ddx = bin / 512; dx <= bin / 2; dx += ddx) {
-                            if (x + dx <= 0) {
-                                continue;
+                        if (err < 2e-31) {
+                            sw.WriteLine($"x={x}, n={n}, |dx| = {ddx}");
+
+                            sw.WriteLine($",i,p_i,q_i");
+                            for (int i = 0; i <= n; i++) {
+                                sw.WriteLine($"f,{i},{fms[i]:e64},{fns[i]:e64}");
+                            }
+                            for (int i = 0; i <= n; i++) {
+                                sw.WriteLine($"g,{i},{gms[i]:e64},{gns[i]:e64}");
                             }
 
-                            (MultiPrecision<Pow2.N16> f_expected, MultiPrecision<Pow2.N16> g_expected) = FGExpandN16.Value(x + dx);
+                            sw.WriteLine($"relative error = {err:e10}\n");
 
-                            MultiPrecision<Pow2.N16> f_actual = PadeSolver<Pow2.N16>.Approx(dx, fms, fns);
-                            MultiPrecision<Pow2.N16> g_actual = PadeSolver<Pow2.N16>.Approx(dx, gms, gns);
+                            is_e31 = true;
 
-                            f_err = MultiPrecision<Pow2.N16>.Max(f_err, MultiPrecision<Pow2.N16>.Abs(f_expected / f_actual - 1));
-                            g_err = MultiPrecision<Pow2.N16>.Max(g_err, MultiPrecision<Pow2.N16>.Abs(g_expected / g_actual - 1));
-                        }
+                            sw.Flush();
 
-                        sw.WriteLine($"relative error f(x)={f_err:e10}, relative error g(x)={g_err:e10}\n");
-
-                        Console.WriteLine($"x={x}, n={n}");
-                        Console.WriteLine($"relative error f(x)={f_err:e10}");
-                        Console.WriteLine($"relative error g(x)={g_err:e10}");
-
-                        if (f_err < 1e-34 && g_err < 1e-34) {
                             break;
                         }
                     }
+
+                    if (!is_e31) {
+                        sw.WriteLine($"{x} not convergence.");
+                    }
                 }
             }
-
-            //using (StreamWriter sw = new("../../../../results_disused/fresnelc_table.csv")) {
-            //    for (double x = 1d / 16; x <= 16; x += 1d / 16) {
-            //        sw.WriteLine($"\"{FresnelN8.FresnelC(x):e40}\",");
-            //    }
-            //}
-            //
-            //using (StreamWriter sw = new("../../../../results_disused/fresnels_table.csv")) {
-            //    for (double x = 1d / 16; x <= 16; x += 1d / 16) {
-            //        sw.WriteLine($"\"{FresnelN8.FresnelS(x):e40}\",");
-            //    }
-            //}
 
             Console.WriteLine("END");
             Console.Read();
